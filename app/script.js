a898dfe51b1ffbf9e4875b3dbfdaedb3d309ce8b
Gabriel <oliveiracgabriel052@gmail.com>
2024-04-15 18:34:18 -0300

    .

--- app/script.js
+++ app/script.js
@@ -1,15 +1,57 @@
+
+
 function limitarDigito(input) {
     if(input.value.length > 1) {
         input.value = input.value.slice(0, 1);
     }
 }
 
-const todosInput = document.querySelectorAll('#input').value
+const botao = document.getElementById('botao');
 
-function inputs() {
-    todosInput.forEach(element => {
-        console.log(element.value)
+botao.addEventListener('click', () => {
+    const inputs = document.querySelectorAll('#input');
+    const valores = [];
+
+    inputs.forEach(input => {
+        valores.push(input.value);
     });
+
+    if (valores.length === 9) {
+        console.log(valores);
+    }
+
+    const primeiroResultado = primeiroCalculo(valores);
+
+    console.log(primeiroResultado[primeiroResultado.length - 1]);
+
+    const n = [];
+    for (let i = 0; i < 9; i++) {
+        n[i] = valores[i];
+    }
+    const resultadoFinalPrimeira = definindoOPrimeiro(primeiroResultado)
+    console.log(resultadoFinalPrimeira);
+});
+
+function primeiroCalculo(valores) {
+    let resultadoPrimeiroCalculo = 0;
+    for (let i = 0; i < valores.length; i++) {
+        resultadoPrimeiroCalculo += valores[i] * (10 - i);
+    }
+
+
+    return [resultadoPrimeiroCalculo];
 }
 
-inputs()
+function definindoOPrimeiro(primeiroResultado) {
+    let resultadoPrimeiro = 0
+    let definindoPrimeiro = primeiroResultado%11
+ 
+    if(definindoPrimeiro < 2) {
+        resultadoPrimeiro = 0
+    } else {
+        resultadoPrimeiro = 11 - definindoPrimeiro
+    }
+
+    return resultadoPrimeiro;
+}
+
